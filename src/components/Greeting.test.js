a7b0c05d94763883d851b2c4202cb23d5580dfed
brameldering <bram.eldering@gmail.com>
2023-07-03 13:45:30 +0200

    Testing with act

---
+++ src/components/Greeting.test.js
@@ -0,0 +1,43 @@
+import { render, screen, act } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Greeting from "./Greeting";
+
+describe("Greeting component", () => {
+  test("renders Hello World as a text", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    // ... nothing
+
+    // Assert
+    const helloWorldElement = screen.getByText("Hello World!");
+    expect(helloWorldElement).toBeInTheDocument();
+  });
+  test("renders good to see you if button was not clicked", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    // ... nothing
+
+    // Assert
+    const outputElement = screen.getByText(`It's good to see you!`, { exact: false });
+    expect(outputElement).toBeInTheDocument();
+  });
+  test(`renders "Changed!" after button was clicked`, async () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    // const buttonElement = screen.getByText("Change Text");
+    act(() => {
+      userEvent.click(buttonElement);
+    });
+
+    // Assert
+    const outputElement = await screen.findByText("Changed!");
+    expect(outputElement).toBeInTheDocument();
+  });
+});
